0eb6a91e4c18e6fd426a54c8c6674ba5099d2ad0
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2024-09-17 15:49:28 -0300

    Implement new tests!

--- tests/Feature/Traits/PayWithWebpayTest.php
+++ tests/Feature/Traits/PayWithWebpayTest.php
@@ -3,21 +3,70 @@
 use SextaNet\LaravelWebpay\Exceptions\MissingAmount;
 use SextaNet\LaravelWebpay\Exceptions\MissingBuyOrder;
 use SextaNet\LaravelWebpay\Exceptions\MissingMarkAsPaidWithWebpay;
+use SextaNet\LaravelWebpay\Traits\PayWithWebpay;
 
 beforeEach(function () {
     $this->stubModelWithTrait = new class {
-        use \SextaNet\LaravelWebpay\Traits\PayWithWebpay;
+        use PayWithWebpay;
     };
 });
 
-it('needs to implement getBuyOrderAttribute()', function () {
-    $this->stubModelWithTrait->getBuyOrderAttribute();
-})->expectException(MissingBuyOrder::class);
+describe('needs to implement getBuyOrderAttribute()', function () {
+    test('not implemented', function () {
+        $this->stubModelWithTrait->getBuyOrderAttribute();
+    })->expectException(MissingBuyOrder::class);
 
-it('needs to implement getAmountAttribute()', function () {
-    $this->stubModelWithTrait->getAmountAttribute();
-})->expectException(MissingAmount::class);
+    test('implemented', function () {
+        $stub = new class {
+            use PayWithWebpay;
+    
+            public function getBuyOrderAttribute(): string
+            {
+                return 1000;
+            }
+        };
 
-it('needs to implement markAsPaidWithWebpay()', function () {
-    $this->stubModelWithTrait->markAsPaidWithWebpay();
-})->expectException(MissingMarkAsPaidWithWebpay::class);
+        expect($stub->getBuyOrderAttribute())
+            ->not->toBeNull();
+    });
+});
+
+describe('needs to implement getAmountAttribute()', function () {
+    test('not implemented', function () {
+        $this->stubModelWithTrait->getAmountAttribute();
+    })->expectException(MissingAmount::class);
+
+    test('implemented', function () {
+        $stub = new class {
+            use PayWithWebpay;
+    
+            public function getAmountAttribute(): string
+            {
+                return 1000;
+            }
+        };
+
+        expect($stub->getAmountAttribute())
+            ->not->toBeNull();
+    });
+});
+
+describe('needs to implement markAsPaidWithWebpay()', function () {
+    test('not implemented', function () {
+        $this->stubModelWithTrait->markAsPaidWithWebpay();
+    })->expectException(MissingMarkAsPaidWithWebpay::class);
+
+    test('implemented', function () {
+        $stub = new class {
+            use PayWithWebpay;
+    
+            public function markAsPaidWithWebpay()
+            {
+                return true;
+            }
+        };
+
+        expect($stub->markAsPaidWithWebpay())
+            ->not->toBeNull();
+    });
+});
