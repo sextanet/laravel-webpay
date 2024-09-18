98749a0351e8499a973c5b5dbf01cd00244d33f2
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2024-09-18 00:02:46 -0300

    Implement VCI descriptions

--- src/Enums/Vci.php
+++ src/Enums/Vci.php
@@ -14,4 +14,20 @@ enum Vci: string
     case EOP = 'EOP';
     case BNA = 'BNA';
     case ENA = 'ENA';
+
+    public function getDescription(): string
+    {
+        return match ($this->value) {
+            self::TSY => 'Autenticación exitosa',
+            self::TSN => 'Autenticación rechazada',
+            self::NP => 'No participa, sin autenticación',
+            self::U3 => 'Falla de conexión, autenticación rechazada',
+            self::INV => 'Datos inválidos',
+            self::A => 'Intentó',
+            self::CNP1 => 'Comercio no participa',
+            self::EOP => 'Error operacional',
+            self::BNA => 'BIN no adherido',
+            self::ENA => 'Emisor no adherido',
+        };
+    }
 }
