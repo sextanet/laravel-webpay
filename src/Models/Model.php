efa3781988ba38379d2d04c176641c3e8ee87f02
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2026-04-13 16:39:19 -0400

    chore: remove abstract model into model

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-abstract class Model extends BaseModel
+class Model extends BaseModel
 {
     use HasFactory;
     use SoftDeletes;
